752765eb85183cd2cce2c859abe4a3be5c66fca2
Eliane202441 <elianetnutricao@gmail.com>
2024-11-21 19:52:58 -0300

    first commit

---
+++ resources/variables.py
@@ -0,0 +1,10 @@
+# variaiveis 
+
+url = 'https://restful-booker.herokuapp.com'
+content_type = 'application/json'
+firstname = 'Elen'
+lastname = 'Santos'
+totalprice = 130
+depositpaid = False 
+bookingdates = dict(checkin='2024-04-27', checkout='2024-04-27')
+additionalneeds = 'Breakfast'
